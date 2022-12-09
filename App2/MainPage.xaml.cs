c849390a66ed80020ce5d06c9c4c3cfdc327f8ad
FLDSMDFR0 <113955168+FLDSMDFR0@users.noreply.github.com>
2022-12-09 15:53:00 -0500

    Add files via upload

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using static Xamarin.Essentials.Permissions;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            string name = Name.Text;
+            string ocupation = Ocupacion.Text;
+            string date = Birthday.Text;
+            string email = Email.Text;
+            string phone = Phone.Text;
+            string nationality = Nacionalidad.Text;
+            string level = Level.Text;
+            string perfil = Perfil.Text;
+            Navigation.PushAsync(new Page1( name, ocupation, date, email, phone, nationality,  level, perfil));
+        }
+    }
+}
---
+++ App2/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Xamarin.Essentials.Permissions;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(string name,string ocupation,string birthday,string email,string phone,string nacionalidad,string level,string perfil)
+        {
+            InitializeComponent();
+            F_nombre.Text = name;
+            F_ocupacion.Text = ocupation;
+            F_edad.Text = birthday + " ; " + name;
+            F_email.Text = "E-mail: " + email;
+            F_telefono.Text = "Telefono: " + phone;
+            F_pais.Text = "Nacionalidad: " + nacionalidad;
+            F_level.Text = "Ingles: " + level;
+            F_perfil.Text = perfil;
+        }
+    private void btnregresar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
